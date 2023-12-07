b0865eb249999c42b5103caed7e4619a456961a6
danny2004de <desaisannidhya43@gmail.com>
2023-12-07 01:38:35 -0600

    day 7 changes

--- adv7.py
+++ adv7.py
@@ -1,32 +1,30 @@
 from functools import total_ordering
-from functools import reduce
-import random
+from collections import Counter
+
 def part1(filename):
     @total_ordering
     class hand:
         def __init__(self, string, bid):
             self.string = string
-            self.cards = dict(reduce(lambda x, y: {**x, y: x.get(y, 0) + 1}, string, {}))
+            self.cards = Counter(string)
             self.bid = int(bid)
-            self.type = 0
-            k = self.cards.values()
-            if 5 in k: self.type = 1
-            elif 4 in k: self.type = 2
+            self.type = 0 # 7 is 5ofak, 1 is junk
+            k = self.cards.values() 
+            if 5 in k: self.type = 7
+            elif 4 in k: self.type = 6
             elif 3 in k: 
-                if 2 in k: self.type = 3
+                if 2 in k: self.type = 5
                 else: self.type = 4
             elif 2 in k:
-                if len([i for i in k if i == 2]) == 2: self.type = 5
-                else: self.type = 6
-            else: self.type = 7
+                if len([i for i in k if i == 2]) == 2: self.type = 3
+                else: self.type = 2
+            else: self.type = 1
                  
         def __eq__ (self, other):
-            return self.cards == other.cards
+            return self.string == other.string
         def __gt__(self, other):
-            if self.type < other.type:
-                return True
-            if self.type > other.type:
-                return False
+            if self.type != other.type:
+                return self.type > other.type
             for i,j in zip(self.string, other.string):
                 selfstrength = '23456789TJQKA'.index(i)
                 otherstrength = '23456789TJQKA'.index(j)
@@ -51,7 +49,7 @@ def part2(filename):
     class hand:
         def __init__(self, string, bid):
             self.string = string
-            self.cards = dict(reduce(lambda x, y: {**x, y: x.get(y, 0) + 1}, string, {}))
+            self.cards = Counter(string)
             jokers = self.cards.pop('J', 0)
             try:
                 m = max(self.cards, key=self.cards.get)
@@ -59,32 +57,31 @@ def part2(filename):
             except:
                 self.cards['2'] = 5
             self.bid = int(bid)
-            self.type = 0
+            self.type = 0 # 7 is 5ofak, 1 is junk
             k = self.cards.values()
-            if 5 in k: self.type = 1
-            elif 4 in k: self.type = 2
+            if 5 in k: self.type = 7
+            elif 4 in k: self.type = 6
             elif 3 in k: 
-                if 2 in k: self.type = 3
+                if 2 in k: self.type = 5
                 else: self.type = 4
             elif 2 in k:
-                if len([i for i in k if i == 2]) == 2: self.type = 5
-                else: self.type = 6
-            else: self.type = 7
+                if len([i for i in k if i == 2]) == 2: self.type = 3
+                else: self.type = 2
+            else: self.type = 1
                  
         def __eq__ (self, other):
             return self.string == other.string
         def __gt__(self, other):
-            if self.type < other.type:
-                return True
-            if self.type > other.type:
-                return False
+            if self.type != other.type:
+                return self.type > other.type
             for i,j in zip(self.string, other.string):
                 selfstrength = 'J23456789TQKA'.index(i)
                 otherstrength = 'J23456789TQKA'.index(j)
-                if selfstrength==otherstrength: continue
+                if selfstrength == otherstrength: continue
                 if selfstrength > otherstrength: return True
                 if selfstrength < otherstrength: return False
             return False
+        
     f = open(filename, 'r')
     lines = f.read().splitlines()
     hands = [hand(*tuple(i.split())) for i in lines]
@@ -94,6 +91,6 @@ def part2(filename):
         rank = i+1
         sum += rank*hands[i].bid
     print(sum)
-    print(hands[-1].string)
     
+part1('input')   
 part2('input')
